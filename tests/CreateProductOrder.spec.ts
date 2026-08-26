00858df2aafb52458ff080cb8bb8dd424b75ec04
Vinicius Barcelos <viniciusbarc@gmail.com>
2026-08-25 23:52:04 -0300

    Pequenos ajustes

--- tests/CreateProductOrder.spec.ts
+++ tests/CreateProductOrder.spec.ts
@@ -1,6 +1,19 @@
 import { expect, test } from "@playwright/test";
 
 test("cadastra um pedido de produto", async ({ page }) => {
+
+  const barcode = Date.now().toString();
+  const name = 'Coca-Cola' + barcode;
+
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Produtos', exact: true }).click();
+  await page.getByRole('link', { name: 'Novo Produto' }).click();
+  await page.waitForTimeout(500);
+  await page.getByRole('textbox', { name: 'Código de Barras' }).fill(barcode);
+  await page.getByRole('textbox', { name: 'Nome' }).fill(name);
+  await page.getByRole('button', { name: 'Criar Produto' }).click();
+  await page.waitForTimeout(500);
+
   await page.goto("/orders/new");
 
   const productSelect = page.getByTestId("product-order-product");
@@ -11,11 +24,12 @@ test("cadastra um pedido de produto", async ({ page }) => {
   await page.getByTestId("product-order-quantity").fill("2");
   await page
     .getByTestId("product-order-date")
-    .fill(new Date().toISOString().slice(0, 10));
+    .fill(new Date("2023-01-01").toISOString().slice(0, 10));
   await page.getByTestId("product-order-submit").click();
 
   await expect(page).toHaveURL(/\/orders\/[^/]+$/);
   await expect(page.getByTestId("product-order-detail")).toBeVisible();
   await expect(page.getByTestId("product-order-detail-quantity")).toHaveText("2");
   await expect(page.getByTestId("product-order-detail-status")).toBeVisible();
+  await expect(page.getByTestId("product-order-detail-status")).toHaveText("opened");
 });
